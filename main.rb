fb2b60136fc397d8590f5b2a60a4e0b10cd78496
Leigha <cdversiontwo@gmail.com>
2020-12-07 00:00:00 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,9 @@
+# p true ^ false ^ true
+
+# p true || false || true
+
+# p ["a", "b", "c"].map { |character| character.upcase }
+
+# p ["a", "b", "c"].map do |character|
+#   character.upcase
+# end
