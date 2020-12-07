c08f1199e67b84544e7dbe8328d942727f0d9e73
Leigha <cdversiontwo@gmail.com>
2020-12-07 00:09:39 +0000

    add result for each example

--- main.rb
+++ main.rb
@@ -1,9 +1,19 @@
 # Create blog series on lesser known operators in Ruby 
 # ^, &., and what else?
 
+# p true ^ true ^ true
+# result is true
+
+# p true || true || true
+# result is true
+
+
 # p true ^ false ^ true
+# result is false
 
 # p true || false || true
+# result is true
+
 
 # p ["a", "b", "c"].map { |character| character.upcase }
 
